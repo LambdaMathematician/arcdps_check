4d4bfdca28adafadbda9feedba0c05c032ac0495
Kagehisa Anotsu <thetoans@hotmail.com>
2020-07-09 23:43:28 -0500

    Moved pauses around and clarified messages. Tried to make instructions diem-proof.

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # arcdps_check
-Made this for my guild and decided to throw it on github for their review and usage.
+Made this for my guild and decided to throw it on github for their review and usage. Uses default paths and file names to work out of the box for most users.
 
-Compares the md5 of the installed d3d9.ll against the one posted in https://www.deltaconnected.com/arcdps/x64/d3d9.dll.md5sum, and downloads the updated version if they differ.
+Compares the md5 of the installed d3d9.ll against the one posted in https://www.deltaconnected.com/arcdps/x64/d3d9.dll.md5sum, and downloads the updated version if they differ. If you don't have arcdps installed, this will install it.
 
-Download the arcdps_check.ps1 file somewhere convenient.
-
-Once downloaded, right click it and select Run With Powershell.
-
-If you don't have arcdps installed, this will install it.
-
-If you'd like to have this as a scheduled task, check out https://social.technet.microsoft.com/wiki/contents/articles/38580.windows-task-scheduler-configure-to-run-a-powershell-script.aspx
+1. Click on the green Code button above to the right and select Download ZIP.
+2. Open the zip file and place the arcdps_check.ps1 file somewhere convenient like your desktop.
+3. To run the script, right click on arcdps_check.ps1 and select Run With Powershell.
+4. Repeat Step 3. whenever you want to make sure you are running the latest arcdps.
+5. (Optional) If you'd like to have this as a scheduled task, remove the lines tagged with #PAUSE and check out https://social.technet.microsoft.com/wiki/contents/articles/38580.windows-task-scheduler-configure-to-run-a-powershell-script.aspx
 
 Lines 2-10 in the script can be removed if you know the $myFile variable is correct.
--- arcdps_check.ps1
+++ arcdps_check.ps1
@@ -1,4 +1,4 @@
-$myFile = "C:\Program Files\Guild Wars 2\bin64\d3d9.dll"
+$myFile = "C:\Program Files\Guild Wars 2\bin64\d3d9.dll" #Location and filename of arcdps dll
 "****************************************************************************************************
 THIS SCRIPT WILL OVERWRITE $myFile.
 IF THE ARCDPS DLL IS NAMED SOMETHING ELSE PLEASE MODIFY THE MYFILE VARIABLE IN THE SCRIPT ACCORDINGLY
@@ -6,7 +6,7 @@ IF THE ARCDPS DLL IS NAMED SOMETHING ELSE PLEASE MODIFY THE MYFILE VARIABLE IN T
 Also note that if someone hacks the arcdps site and puts malicious code in the dll, it ain't my fault.
 You probably would have just downloaded and ran it manually.
 Feel free to delete these lines and in the script once you understand the risks."
-Pause #Delete this line if you want it to just run without interruption at the beginning
+""
 
 # Get the md5 of the installed arcdps
 $md5Command = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
@@ -19,22 +19,25 @@ Else {$installed_hash = "N/A. $myFile does not exist."}
 # Get the md5 posted on the arcdps site
 $web_hash = (Invoke-WebRequest 'https://www.deltaconnected.com/arcdps/x64/d3d9.dll.md5sum').ToString().Remove(32)
 "Web Hash      : $web_hash"
+""
 
-If ($installed_hash -eq $web_hash) { echo "You are up to date" }
+If ($installed_hash -eq $web_hash) { echo "You are already on the latest arcdps." }
 Else {
-    echo "You are not up to date... Downloading"
+    Read-Host -Prompt "New version available... Press ENTER to update or CTRL+C to exit the script" #PAUSE
+    "Downloading new version..."
     Invoke-WebRequest 'https://www.deltaconnected.com/arcdps/x64/d3d9.dll' -OutFile $myFile
 
     #make sure the download was good
     $installed_hash = [System.BitConverter]::ToString($md5Command.ComputeHash([System.IO.File]::ReadAllBytes($myFile))).Replace("-","").ToLower()
 
-    If ($installed_hash -eq $web_hash) {"Validated download. You are now up to date."}
+    If ($installed_hash -eq $web_hash) {"Validated download. arcdps is updated."}
     Else {
-		"MD5 of downloaded file does not match published MD5 sum."
+		"Something's gone wrong. MD5 of downloaded file does not match published MD5 sum."
 	    "Possible reason:"
-	    "    Could not write to $myFile. Run again as admin/Grant permission to write to the folder."
+	    "    Could not write to $myFile. Make sure GW2 is not running and you have permission to write to the folder."
 	    "    Download was corrupted somehow. Run again."
 	    "    New MD5 sum was not published. Extremely unlikely. Proceed with caution."
-	    }
     }
-Pause
+}
+""
+Read-Host -Prompt "Finished script. Press ENTER to exit." #PAUSE
